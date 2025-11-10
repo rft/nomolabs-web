88af6bcc3ca7a051646d07ada90b786ae931a05a
rft <nano@nomolabs.net>
2025-11-10 02:55:31 +0000

    basic navbar

--- nomolabs-web/src/routes/+layout.svelte
+++ nomolabs-web/src/routes/+layout.svelte
@@ -1,12 +1,37 @@
-<script lang="ts">
-	import '../app.css';
-	import favicon from '$lib/assets/favicon.svg';
+<script>
+  import {
+    Column,
+    Content,
+    Grid,
+    Header,
+    HeaderNav,
+    HeaderNavItem,
+    HeaderNavMenu,
+    Row,
+    SkipToContent,
+  } from "carbon-components-svelte";
 
-	let { children } = $props();
+  let isSideNavOpen = false;
 </script>
 
-<svelte:head>
-	<link rel="icon" href={favicon} />
-</svelte:head>
+<Header company="" platformName="Nomolabs" bind:isSideNavOpen>
+  <svelte:fragment slot="skip-to-content">
+    <SkipToContent />
+  </svelte:fragment>
+  <HeaderNav>
+    <HeaderNavItem href="/" text="Blog" />
+    <HeaderNavItem href="/" text="Gallery" />
+    <HeaderNavItem href="/" text="Tools" />
+  </HeaderNav>
+</Header>
 
-{@render children()}
+<Content>
+  <Grid>
+    <Row>
+      <Column>
+        <h1>Welcome</h1>
+      </Column>
+    </Row>
+  </Grid>
+</Content>
+
--- readme.md
+++ readme.md
@@ -3,7 +3,15 @@
 ## Nix flake explained
 
 ### Getting started
+
+To enter a development shell run:
 `nix develop`
+once inside the shell run:
+`npm install` to install dependencies
+and then
+`npm run dev` to start the development server
 
 ### Building docker image
+
+`nix build .#dockerImage`
 
